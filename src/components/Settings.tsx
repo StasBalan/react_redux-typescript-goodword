2dc15c8195e3700fa2abaac627e73b7e31308285
StasBalan <43076181+StasBalan@users.noreply.github.com>
2019-07-08 14:11:01 +0300

    updated Settings component

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
-class Settings extends Component {
+interface SettingsState {
+    range: number;
+};
+
+class Settings extends Component<{}, SettingsState> {
+
+    state: SettingsState = {
+        range: 1
+    };
+
+    private handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let value = Number(e.target.value);
+        console.log('save range',value, typeof value);
+        this.setState({
+            range: value
+        });
+    }
+
+    private onSave = () => {
+        console.log('you save settings', this.state.range);
+    }
+
     render() {
+        const { range } = this.state;
+
         return (
             <div>
-                Setttings
+                <div>
+                    <p>{range}</p>
+                    <input onChange={this.handleChange}
+                        value={range} 
+                        type="range" 
+                        min='1' 
+                        max='12' 
+                        step='1'/>
+                </div>
+                <button onClick={this.onSave}>SAVE</button>
             </div>
         );
     }
