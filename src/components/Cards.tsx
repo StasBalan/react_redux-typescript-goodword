73ff07f49d0dc2d4c0b1cdbdb1e2c140dd6ac766
StasBalan <43076181+StasBalan@users.noreply.github.com>
2019-07-10 17:44:01 +0300

    modified Cards component and constant

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 import { AppState } from '../store/store';
+import CardsItems from './CardsItems';
+import { KEY } from '../constants/constants';
 
 import { connect } from 'react-redux';
 import { showingCards } from '../actions/loader/actions';
 
-
+interface CardsState {
+    card: any[]
+}
 
 interface AppProps {
     wordToLearn: string[];
@@ -13,30 +17,52 @@ interface AppProps {
     showingCards: () => void;
 }
 
-class Cards extends Component<AppProps> {
+class Cards extends Component<AppProps, CardsState> {
 
-    componentDidMount() {
+    state: CardsState = {
+       card: [] 
+    }
+
+    private onShow = () => {
         const wordToLearn = this.props.wordToLearn;
-        const key = 'b94e78e4-8545-4c28-bd32-2b331aede418';
+        console.log('пришел массив в Cards: ', wordToLearn);
         wordToLearn.forEach((el) => {
-            fetch(`https://www.dictionaryapi.com/api/v3/references/sd4/json/${el}?key=${key}`)
-            .then(() => this.props.showingCards());
+            fetch(`https://www.dictionaryapi.com/api/v3/references/sd4/json/${el}?key=${KEY}`)
+                .then((res: any) => res.json())
+                .then((data) => this.onShowFetching(data));
         })
+    }
+
+    private onShowFetching = (arr: any[]) => {
+        const fetchingArray: any[] = [];
+        const card: any[] = [];
+        fetchingArray.push(arr[0]);  
+        console.log('массив fetchingArray: ', fetchingArray, arr[0]);      
+        fetchingArray.forEach((el) => {
+           card.push({title: el.meta.stems[0], description: el.shortdef[0]})
+        });
+        console.log('массив card: ', card);
+        this.setState({
+            card: card
+        });
     }
 
     render() {
         const { loader } = this.props;
+        const { card } = this.state;
 
         return(
             <div>
-                {loader ? <div>Loading...</div>:<CardsItems/>}
+                <button onClick={this.onShow}>Show</button>
+                <CardsItems card={card}/>
             </div>
         );
     }
 }
 
 const mapStateToProps = (state: AppState) => ({
-    wordToLearn: state.vocabulary.wordToLearn 
+    wordToLearn: state.vocabulary.wordToLearn,
+    loader: state.loader.loader 
 })
 
 export default connect(mapStateToProps, { showingCards }) (Cards);
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,2 @@
 export const INITIAL_RANGE = 1;
+export const KEY = 'b94e78e4-8545-4c28-bd32-2b331aede418';
