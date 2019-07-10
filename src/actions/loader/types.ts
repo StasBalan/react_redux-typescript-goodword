6f9eab97e47d8673b8ba8ce981e8da0e08ea36cb
StasBalan <43076181+StasBalan@users.noreply.github.com>
2019-07-10 13:14:30 +0300

    modified loader reducer

--- src/actions/loader/types.ts
+++ src/actions/loader/types.ts
@@ -1,5 +1,9 @@
 export const SHOWING_CARDS = 'SHOWING_CARDS';
 
+export interface LoaderState {
+    loader: Boolean;
+} 
+
 interface showingCardsAction {
     type: typeof SHOWING_CARDS;
     payload: Boolean;
--- src/reducers/loaderReducer.ts
+++ src/reducers/loaderReducer.ts
@@ -1,9 +1,15 @@
-const initialState = {
+import {LoaderState, LoaderActionTypes, SHOWING_CARDS} from '../actions/loader/types';
+
+const initialState: LoaderState = {
     loader: false
 }
 
-export function loaderReducer(state = initialState, action ) {
+export function loaderReducer(state = initialState, action: LoaderActionTypes ): LoaderState {
     switch(action.type) {
+        case SHOWING_CARDS:
+            return {
+                loader: !state.loader
+            } 
         default:
             return state
     }
