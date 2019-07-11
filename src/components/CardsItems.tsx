22cdda39c37be83cb5b076e970c6960ba67bb3f4
StasBalan <43076181+StasBalan@users.noreply.github.com>
2019-07-11 13:28:20 +0300

    refactoring CardsItems component

--- src/components/CardsItems.tsx
+++ src/components/CardsItems.tsx
@@ -4,29 +4,27 @@ interface CardsItemsProps {
     card: any[];
 }
 
-class CardsItems extends Component<CardsItemsProps> {
-    render(){
-        const { card } = this.props;
+const CardsItems: React.FC<CardsItemsProps> = ({card}) => {
         
-        const elementsCard = card.map((el, index) => {
-            return (
-                <li key={index} style={{listStyle: 'none',
-                                        width: '300px',
-                                        backgroundColor: '#282c34', 
-                                        color: '#ffffff',
-                                        padding: '5px',
-                                        margin: '10px auto'}}>
-                    <p>{el.title}</p>
-                    <p>{el.description}</p>
-                </li>
-            );
-        });
-        return(
-            <ul>
-                {elementsCard}
-            </ul>
+    const elementsCard = card.map((el, index) => {
+        return (
+            <li key={index} style={{listStyle: 'none',
+                                    width: '300px',
+                                    backgroundColor: '#282c34', 
+                                    color: '#ffffff',
+                                    padding: '5px',
+                                    margin: '10px auto'}}>
+                <p>{el.title}</p>
+                <p>{el.description}</p>
+            </li>
         );
-    }
+    });
+    
+    return(
+        <ul>
+            {elementsCard}
+        </ul>
+    );
 }
 
 export default CardsItems;
