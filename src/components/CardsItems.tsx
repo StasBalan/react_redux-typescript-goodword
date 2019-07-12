0cc3b45794ffc19edeb2a1b227b429f98609f7d1
StasBalan <43076181+StasBalan@users.noreply.github.com>
2019-07-12 10:26:05 +0300

    rewrote CardsItems on the class component

--- src/components/CardsItems.tsx
+++ src/components/CardsItems.tsx
@@ -4,27 +4,30 @@ interface CardsItemsProps {
     card: any[];
 }
 
-const CardsItems: React.FC<CardsItemsProps> = ({card}) => {
+class CardsItems extends Component<CardsItemsProps> {
+    render() {
+        const { card } = this.props;
+
+        const elementsCard = card.map((el, index) => {
+            return (
+                <li key={index} style={{listStyle: 'none',
+                                        width: '300px',
+                                        backgroundColor: '#282c34', 
+                                        color: '#ffffff',
+                                        padding: '5px',
+                                        margin: '10px auto'}}>
+                    <p>{el.title}</p>
+                    <p>{el.description}</p>
+                </li>
+            );
+        });
         
-    const elementsCard = card.map((el, index) => {
-        return (
-            <li key={index} style={{listStyle: 'none',
-                                    width: '300px',
-                                    backgroundColor: '#282c34', 
-                                    color: '#ffffff',
-                                    padding: '5px',
-                                    margin: '10px auto'}}>
-                <p>{el.title}</p>
-                <p>{el.description}</p>
-            </li>
+        return(
+            <ul>
+                {elementsCard}
+            </ul>
         );
-    });
-    
-    return(
-        <ul>
-            {elementsCard}
-        </ul>
-    );
+    }
 }
 
 export default CardsItems;
