150b0e40a8e4654fe85065c1e8d71b8944ed0ab5
StasBalan <43076181+StasBalan@users.noreply.github.com>
2019-07-12 10:04:33 +0300

    added CardInfo component

---
+++ src/components/CardInfo.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CardInfo extends Component {
+    render() {
+        return(
+            <div>
+                Card Info
+            </div>
+        );
+    }
+}
+
+export default CardInfo;
