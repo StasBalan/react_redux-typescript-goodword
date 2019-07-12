9c92041b2965da30d31ab93b276ed92a4dc9d43f
StasBalan <43076181+StasBalan@users.noreply.github.com>
2019-07-12 10:02:03 +0300

    added key for api photo

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,3 @@
 export const INITIAL_RANGE = 1;
 export const KEY = 'b94e78e4-8545-4c28-bd32-2b331aede418';
+export const KEY_FOR_PHOTO_API = '3600e79e752283c702d8928b0abbac28e7b0bd1b3108dfdb1d4c091a2b46a01d';
