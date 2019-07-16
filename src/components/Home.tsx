c5c3a58afa6dedab44e47a63fcc878a34616ba7d
StasBalan <43076181+StasBalan@users.noreply.github.com>
2019-07-16 17:13:14 +0300

    added image and styles for home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,6 +10,7 @@ const Home: React.FC = () => {
             <Header/>
             <div className='container'>
                 <h1 className='title'>Welocme to our<br/>application for learning<br/>English word</h1>
+                <img className='home__img' src="https://www.onlygfx.com/wp-content/uploads/2016/08/flag-of-uk.png" alt="flag"/>
             </div>
         </div>
     );
